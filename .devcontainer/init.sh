4f68f1116e270f798ebbe53537e82c6854f5215c
Can Demiralp <can.demiralp@adyen.com>
2025-10-20 20:50:34 +0200

    Update image

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 service mariadb restart
+
+DB_NAME="magento"
+DB_USER="magento"
+DB_PASS="magento"
+
+mariadb -e "
+  CREATE DATABASE $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+  CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';
+  GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
+  FLUSH PRIVILEGES;"
+
 exec apache2-foreground
