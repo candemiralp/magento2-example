1085ce19f6239840961964b1cd8fcb7d4266f727
Can Demiralp <can.demiralp@adyen.com>
2025-10-20 21:12:11 +0200

    Update image

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -12,6 +12,15 @@ mariadb -e "
   GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
   FLUSH PRIVILEGES;"
 
+if [[ -e /usr/local/bin/composer ]]; then
+	echo "Composer already exists"
+else
+	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+	php composer-setup.php --quiet
+	rm composer-setup.php
+	mv composer.phar /usr/local/bin/composer
+fi
+
 cd /var/www/html
 
 wget https://github.com/magento/magento2/archive/refs/tags/2.4.8-p3.tar.gz
